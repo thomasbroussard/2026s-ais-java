fc10f0a7f809c169e197cb7289ae003e7b1438e4
thomasbroussard <thomas@contactoffice.com>
2026-05-26 15:01:55 +0200

    * coverage + debugging

--- bank/test/fr/epita/bank/test/TestBankAccountService.java
+++ bank/test/fr/epita/bank/test/TestBankAccountService.java
@@ -1,7 +1,10 @@
 package fr.epita.bank.test;
 
 
+import fr.epita.bank.datamodel.InvestmentAccount;
 import fr.epita.bank.datamodel.SavingsAccount;
+import fr.epita.bank.datamodel.Stock;
+import fr.epita.bank.datamodel.StockOrder;
 import fr.epita.bank.services.BankAccountService;
 
 public class TestBankAccountService {
@@ -9,10 +12,30 @@ public class TestBankAccountService {
 
     //TDD
     public static void main(String[] args) {
-        //checkBankAccountServiceComputeInterest();
+        checkBankAccountServiceComputeInterest();
+        checkBankAccountServiceBuyStock();
 
 
+    }
 
+    private static void checkBankAccountServiceBuyStock() {
+        //initialization
+        InvestmentAccount account = new InvestmentAccount();
+        account.setBalance(10000.0);
+        Stock stock = new Stock();
+        stock.setTicker("GOLD");
+        stock.setCurrentPrice(1800.0);
+        int quantity = 3;
+
+        //action
+        StockOrder stockorder = BankAccountService.buyStock(account, stock, quantity);
+
+        //verification
+        if (Math.abs(account.getBalance() - 4600) < 0.0001){
+            System.out.println("checkBankAccountServiceBuyStock: test passed");
+        }else {
+            System.out.println("checkBankAccountServiceBuyStock: test failed");
+        }
     }
 
     private static void checkBankAccountServiceComputeInterest() {
@@ -32,16 +55,16 @@ public class TestBankAccountService {
         double expectedGain = initialBalance * interestRate;
         System.out.println("gain is " + gain);
         if (Math.abs(expectedGain - gain) < 0.0001 ){
-            System.out.println("test passed");
+            System.out.println("checkBankAccountServiceComputeInterest#gainCheck: test passed");
         }else{
-            System.out.println("test failed");
+            System.out.println("checkBankAccountServiceComputeInterest#gainCheck: test failed");
         }
 
         Double balance = savingsAccount.getBalance();
         if (balance == initialBalance + expectedGain){
-            System.out.println("test passed");
+            System.out.println("checkBankAccountServiceComputeInterest#balanceCheck: test passed");
         }else {
-            System.out.println("test failed");
+            System.out.println("checkBankAccountServiceComputeInterest#balanceCheck: test failed");
         }
     }
 
