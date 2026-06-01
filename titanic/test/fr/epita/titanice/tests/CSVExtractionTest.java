2ce25925e4fc65fa7a0564ce88af15fbd2103094
thomasbroussard <thomas.broussard@gmail.com>
2026-06-01 14:04:36 +0200

    * titanic introduction

--- titanic/test/fr/epita/titanice/tests/CSVExtractionTest.java
+++ titanic/test/fr/epita/titanice/tests/CSVExtractionTest.java
@@ -18,6 +18,19 @@ public class CSVExtractionTest {
         scanner.nextLine();
         while (scanner.hasNext()){
             String line = scanner.nextLine();
+            String[] parts = line.split(";");
+            String name = parts[0];
+            String passengerClass = parts[1];
+            if (parts[2] == null){
+                parts[2] = "0";
+            }
+            String part = parts[2];
+            Double age = Double.parseDouble(part);
+            String sex = parts[3];
+            if (parts[4] == null){
+                parts[4] = "0";
+            }
+            Integer survived = Integer.parseInt(parts[4]);
 
         }
         scanner.close();
